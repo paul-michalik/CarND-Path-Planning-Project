43e8048c5fa9bcf5806c36d3fd462f6343a00d6f
Paul Michalik <paul.michalik@outlook.com>
2018-02-03 12:57:06 +0100

    Fix Docker script. Drives may now contain colons.

--- docker/run-docker.bat
+++ docker/run-docker.bat
@@ -7,7 +7,8 @@ set "CurPath=%CurPath:~0,-1%"
 :: Replace '\' by '/'
 set "CurPath=%CurPath:\=/%"
 :: Remove ':', C:/Users becomes C/Users
-set "CurPath=%CurPath::=%"
+:: set "CurPath=%CurPath::=%"
+:: Wow, obviously this is obsolete with 17.12.0-ce-win47 (15139)
 
 call "%~dp0docker-id.bat"
 docker run -d --rm -p 4567:4567 -p 22:22 --name %DockerContainerName% -v %CurPath%/..:/usr/src/%DockerProject% -w /usr/src/%DockerProject% %DockerUserName%/%DockerImageName%:%DockerImageTag%
